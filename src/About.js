86fc10cad8e046d8a9048102e80c4557cd43520b
Yunyu Lin <mail@yunyul.in>
2019-02-15 11:41:32 -0500

    Add misc section styling

--- src/About.js
+++ src/About.js
@@ -2,11 +2,19 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import { Planet } from './Planet.js';
+import { Section } from './Section.js';
+
 import AboutPlanet from './img/about-planet.svg';
 
 export const About = () => (
   <>
     <Planet src={AboutPlanet} />
-    <div>Ayy lmao</div>
+    <Section title="What is IDEATE?">
+      <p>
+        To explore a wide variety of possible solutions through generating a
+        large quantity of diverse possible solutions, allowing you to step
+        beyond the obvious and explore a range of ideas.
+      </p>
+    </Section>
   </>
 );
---
+++ src/Section.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export const SectionTitle = styled.h1`
+  margin: 0 auto 40px;
+  font-size: 1.5em;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  display: block;
+`;
+
+const SectionContent = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+`;
+
+const SectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px 0;
+`;
+
+export const Section = ({ title, children }) => (
+  <SectionWrapper>
+    <SectionTitle>{title}</SectionTitle>
+    <SectionContent>{children}</SectionContent>
+  </SectionWrapper>
+);
