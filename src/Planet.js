7e02771b8eaade3b05c664144b5b5a8f6bdf1f1a
Yunyu Lin <mail@yunyul.in>
2019-02-16 10:01:09 -0500

    Add wider arc

--- src/Planet.js
+++ src/Planet.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import OrbitArc from './img/orbit-arc.svg';
+import OrbitArc from './img/orbit-arc-wide.svg';
 
 const PlanetWrapper = styled.div`
   position: relative;
   display: flex;
   transform: translateY(50%);
+  margin-top: 60px;
 `;
 
 const Orbit = styled.div`
   position: absolute;
   width: 100%;
-  height: 771px;
-  top: -771px;
+  height: 184px;
+  top: -179px;
   background-position: center;
   background-image: url(${OrbitArc});
+  background-repeat: no-repeat;
 `;
 
 const PlanetImage = styled.img`
-  margin: 0 auto;
+  margin: auto;
   transform: translateY(-50%);
 `;
 
