3afa9bd1c325099812401870be8e2ed7c21712a4
Yunyu Lin <mail@yunyul.in>
2019-02-18 10:18:43 -0500

    Fix about responsiveness

--- src/About.js
+++ src/About.js
@@ -6,51 +6,46 @@ import { Section } from './Section.js';
 
 import AboutPlanet from './img/about-planet.svg';
 
-
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
   grid-row-gap: 20px;
-  grid-auto-rows: minmax(100px, auto);
-  width: 50%;
+  max-width: 640px;
   margin: 0 auto;
-
 `;
 
 const GridItem = styled.div`
   grid-row: auto;
   grid-column: auto;
-  font-weight: 300;
 `;
 
 const Header = styled.h3`
-  font-weight: bold;
+  font-weight: 600;
 `;
 
 export const About = () => (
   <>
     <Planet src={AboutPlanet} />
     <Section title="What is IDEATE?">
-    <GridContainer>
+      <GridContainer>
         <GridItem>
-          <Header>
-            Ideate | īdēˌāt | vb.
-          </Header>
+          <Header>Ideate | īdēˌāt | vb.</Header>
           <p>
-            To explore a wide variety of possible solutions through generating a large quantity of diverse possible soutions, allowing you to step beyond the obvious and explore a range of ideas.
-            </p>
+            To explore a wide variety of possible solutions through generating a
+            large quantity of diverse possible soutions, allowing you to step
+            beyond the obvious and explore a range of ideas.
+          </p>
         </GridItem>
         <GridItem>
-          <Header>
-            Ideate @ Duke is...
-          </Header>
+          <Header>Ideate @ Duke is...</Header>
           <p>
-            A one-day conference filled with awesome speakers and workshops to explore how design and innovation interact. Ideate aims to observe how design-thinking can be applied across field, and is open to design aficionados, curious newcomers, and everyone in between!
-            </p>
+            A one-day conference filled with awesome speakers and workshops to
+            explore how design and innovation interact. Ideate aims to observe
+            how design-thinking can be applied across field, and is open to
+            design aficionados, curious newcomers, and everyone in between!
+          </p>
         </GridItem>
-
       </GridContainer>
-
     </Section>
   </>
 );
