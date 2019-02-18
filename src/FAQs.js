08c93a5bd5c70311b962f33a204a4899c2ba360c
Yunyu Lin <mail@yunyul.in>
2019-02-18 15:15:11 -0500

    Drop FAQ row gap on mobile

--- src/FAQs.js
+++ src/FAQs.js
@@ -9,14 +9,15 @@ import FAQsPlanet from './img/faqs-planet.svg';
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-  grid-row-gap: 50px;
-  grid-column-gap:30px;
+  grid-gap: 30px;
   grid-auto-rows: minmax(100px, auto);
   width: 100%;
 
   @media screen and (min-width: 480px) {
+    grid-row-gap: 50px;
     grid-template-columns: repeat(2, 1fr);
   }
+
   @media screen and (min-width: 768px) {
     grid-template-columns: repeat(3, 1fr);
   }
@@ -38,56 +39,63 @@ export const FAQs = () => (
     <Section title="FAQs">
       <GridContainer>
         <GridItem>
-          <Header>
-            Is this event open for everyone?
-          </Header>
+          <Header>Is this event open for everyone?</Header>
           <p>
-            Due to venue capacity, we are only able to accept the first 200 that apply. Registration will be open shortly!
-            </p>
+            Due to venue capacity, we are only able to accept the first 200 that
+            apply. Registration will be open shortly!
+          </p>
         </GridItem>
         <GridItem>
-          <Header>
-            Is transportation provided?
-          </Header>
+          <Header>Is transportation provided?</Header>
           <p>
-            Duke has transportation from both West and East Campus to Gross Hall. From the chapel, it's less than a 10 minute walk. If you are driving, there is the Bryan Center parking lot nearby!
-            </p>
+            Duke has transportation from both West and East Campus to Gross
+            Hall. From the chapel, it's less than a 10 minute walk. If you are
+            driving, there is the Bryan Center parking lot nearby!
+          </p>
         </GridItem>
         <GridItem>
-          <Header>
-            Do I need to bring anything specific to the event?
-          </Header>
+          <Header>Do I need to bring anything specific to the event?</Header>
           <p>
             Bring your laptop, student ID/government ID, and your design soul!
-            </p>
-        </GridItem>
-        <GridItem>
-          <Header>
-            Lorem ipsum dolor sit amet
-          </Header>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </p>
         </GridItem>
         <GridItem>
-          <Header>
-            Lorem ipsum dolor sit amet
-          </Header>
+          <Header>Lorem ipsum dolor sit amet</Header>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
           </p>
         </GridItem>
         <GridItem>
-          <Header>
-            Lorem ipsum dolor sit amet
-          </Header>
+          <Header>Lorem ipsum dolor sit amet</Header>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
           </p>
         </GridItem>
-
+        <GridItem>
+          <Header>Lorem ipsum dolor sit amet</Header>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </GridItem>
       </GridContainer>
-
     </Section>
   </>
 );
