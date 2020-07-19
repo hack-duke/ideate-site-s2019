f5909f107a083ecad0b0bebc4ac732f583701a7e
Vicki Lu <vicki.lu.777@gmail.com>
2020-07-18 23:28:29 -0400

    schedule

--- src/sections/Schedule/Schedule.js
+++ src/sections/Schedule/Schedule.js
@@ -2,14 +2,53 @@ import React from 'react';
 import Section from 'components/Section';
 import { MainHeader } from 'components/typography';
 
+import ScheduleItemCard from './ScheduleItemCard';
+import scheduleInfo from './scheduleInfo';
+
 import palette from 'assets/palette';
+import styled from 'styled-components';
 
-const Schedule = () => {
-  return (
-    <Section sectionColor={palette.red} sectionId="schedule">
-      <MainHeader style={{ color: palette.white }}>SCHEDULE</MainHeader>
-    </Section>
-  );
-};
 
-export default Schedule;
+export default class Schedule extends React.Component {
+  render() {
+    const saturday = scheduleInfo[0];
+    const sunday = scheduleInfo[1];
+
+    return (
+      <Section sectionColor={palette.red} sectionId="schedule">
+        <MainHeader style={{ color: palette.white }}>SCHEDULE</MainHeader>
+          <ScheduleContainer>
+            <Day>
+              <DateHeader>Saturday July 25</DateHeader>
+              <ScheduleItemCard events={saturday.events}/>
+            </Day>
+
+            <Day>
+              <DateHeader>Sunday July 26</DateHeader>
+              <ScheduleItemCard events={sunday.events}/>
+            </Day>
+          </ScheduleContainer>
+      </Section>
+    );
+  }
+}
+
+const ScheduleContainer = styled('div')`
+  width: 1100px;
+  margin: 75px auto;
+  text-align: center;
+
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Day = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+const DateHeader = styled('div')`
+  color: white;
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 32px;
+`;
---
+++ src/sections/Schedule/scheduleInfo.js
@@ -0,0 +1,179 @@
+export default [
+  {
+    // Months are zero-indexed for whatever reason
+    date: new Date(2020, 7 - 1, 25),
+    events: [
+      {
+        start: '12:00PM',
+        end:'12:15PM',
+        name: 'Opening Ceremony',
+        description: 'Welcome to IDEATE!',
+        additional:'',
+      },
+      {
+        start: '12:15PM',
+        end:'1:00PM',
+        name: 'Keynote Speaker– TBD',
+        description: '',
+        additional:'',
+      },
+      {
+        start: '1:00PM',
+        end:'1:30PM',
+        name: 'Team Formation',
+        description: 'Join us on Glimpse to find teammates',
+        additional:'',
+      },
+      {
+        start: '1:00PM',
+        end:'1:30PM',
+        name: 'Office Hours',
+        description: 'Ask questions about the design challenge',
+        additional:'',
+      },
+      {
+        start: '1:30PM',
+        end:'2:00PM',
+        name: 'Design-Thinking Workshop',
+        description: 'with Dorothy Feng and Kiyoon Ko',
+        additional:'Design Thinking as a concept makes perfect sense as a principle but how is it actually used? Join us in learning how IDEO, Facebook, and other design teams apply Design Thinking in their everyday problem solving!',
+      },
+      {
+        start: '2:00PM',
+        end:'2:30PM',
+        name: 'Prototyping + Components in Figma Workshop',
+        description: 'with Megan Woodruff',
+        additional:'Figma is rapidly becoming one of the most popular tools for UX design + prototyping in the tech industry. Luckily, it\'s also incredibly easy to learn and use! In this workshop, we\'ll cover the basics of Figma\'s prototyping capabilities, and introduce some of the ways that Figma components can be used to enhance your prototyping process.',
+      },
+      {
+        start: '2:30PM',
+        end:'3:00PM',
+        name: 'Open up the door, I\'ll get it myself',
+        description: 'with Whitney Robinson',
+        additional:'What do you do when you stumble upon a problem, feel drawn to act, yet there are no clear next steps? This a talk on how a techie created a design movement in maternal health, and you can too.',
+      },
+      {
+        start: '2:30PM',
+        end:'3:00PM',
+        name: 'Design and Social Justice',
+        description: 'with Tania Anaissie',
+        additional:'What is the role of a designer in an oppressive society? How can design be applied towards social justice and collective liberation? How do we need to adapt design to do this work? Join us to learn about Liberatory Design and the emerging Equity Design movement.',
+      },
+      {
+        start: '3:00PM',
+        end:'3:30PM',
+        name: 'Prototyping Workshop',
+        description: '',
+        additional:'',
+      },
+      {
+        start: '3:30PM',
+        end:'4:00PM',
+        name: 'Design Principles Workshop',
+        description: '',
+        additional:'',
+      },
+      // {
+      //   start: '4:00PM',
+      //   end:'4:30PM',
+      //   name: 'Speaker Talk',
+      //   description: 'with Shreya Shankar',
+      //   additional:,
+      // },
+      {
+        start: '4:00PM',
+        end:'4:30PM',
+        name: 'Just Like Me: Why Creators of Education Technology Should Mirror Their Users’ Backgrounds',
+        description: 'with Melvin Hines',
+        additional:'',
+      },
+      {
+        start: '5:00PM',
+        end:'5:30PM',
+        name: 'Glimpse Social',
+        description: '',
+        additional:'',
+      },{
+        start: '5:30PM',
+        end:'6:00PM',
+        name: 'Storytelling as a Designer',
+        description: 'with Kim Huynh and Carolyn Sun',
+        additional: 'Storytelling isn’t just for writers—it’s an essential skill for anyone in tech, especially designers. We’ll talk about why storytelling is important and how to do it, whether you’re interviewing, pitching, or putting together your design portfolio.'
+      },
+      {
+        start: '6:00PM',
+        end:'6:30PM',
+        name: 'How to Work Together',
+        description: 'with Erin McCue',
+        additional:'A fellow designer once told me that Product Design is a team sport. How can we bring together the expertise, perspectives, and personalities of a team to make stuff and have fun doing it?'
+      },
+      {
+        start: '6:00PM',
+        end:'6:30PM',
+        name: 'Speaker Talk',
+        description: 'with Sanjana Galgalikar',
+        additional:'I would like to speak about my experience joining a 10 person startup as the only designer after graduation and how it compares to my experiences at design agencies and larger companies. I hope to share insight into the different types of work environments/hiring process/deliverables/work styles needed for each and give students with a better idea of what work environment might best align with their goals.'
+      },
+      {
+        start: '7:00PM',
+        end:'10:00PM',
+        name: 'Office Hours',
+        description: 'Get design help from our mentors and team',
+        additional:'',
+      },
+    ]
+  },
+  {
+    date: new Date(2020, 7 - 1, 26),
+    events: [
+      {
+        start: '12:00PM',
+        end:'12:30PM',
+        name: 'Designing Healthcare Applications During a Pandemic',
+        description: 'with Jamal Cromity',
+        additional:'So what is it like working for the nations largest health insurer during the Covid-19 pandemic? Learn how Optum applies design thinking to their agile development process.'
+      },
+      {
+        start: '12:00PM',
+        end:'12:30PM',
+        name: 'Towards the Democratization of Design with AI',
+        description: 'with Tony Beltramelli',
+        additional:'Organizations around the world know and understand the value of design as a core part of their business, leading to the success or the failure of their products and services. However, as design processes become more complex and start involving a lot of different profiles within companies, design tools are becoming a bottleneck due to their complexity and steep learning curves. In this talk, we will explore AI’s potential to democratize design by making tools accessible to everyone in an organization, thus enabling a more seamless collaboration between designers, managers, executives, and developers.'
+      },
+      {
+        start: '12:30PM',
+        end:'4:00PM',
+        name: 'Office Hours',
+        description: 'Get design help from our mentor and team',
+        additional:'',
+      },
+      {
+        start: '4:00PM',
+        end:'',
+        name: 'Submissions Due on Devpost',
+        description: 'devpost link',
+        additional:'',
+      },
+      {
+        start: '4:00PM',
+        end:'6:00PM',
+        name: 'Judging',
+        description: '',
+        additional:'',
+      },
+      // {
+      //   start: '5:00PM',
+      //   end:'5:30PM',
+      //   name: 'Speaker Talk',
+      //   description: 'with'
+      // },
+      {
+        start: '6:00PM',
+        end:'7:00PM',
+        name: 'Closing Ceremony and Awards',
+        description: 'Join us on Youtube Live to wrap up a great weekend!',
+        additional:'',
+      },
+    ]
+  }
+];
