d19ede371de98ba76981d45c5a05916f42944d85
Grant Kim <grant.kim@duke.edu>
2020-07-21 09:23:42 -0500

    see more in schedule

--- src/sections/Schedule/ScheduleItemCard.js
+++ src/sections/Schedule/ScheduleItemCard.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import { BodyHeader, BodyText } from 'components/typography';
 
 import styled from 'styled-components';
 import palette from 'assets/palette';
+import ScheduleRow from './ScheduleRow';
 
 class ScheduleItemCard extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleOpen = () => {
+    this.setState((prevState, prevProps) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
+
   render() {
     const events = this.props.events;
 
     return (
       <CardBase>
-        {events.map(({ start, end, name, description }) => (
-          <EventRow>
-            <EventTime>
-              {start}
-              <br />
-              {end}
-            </EventTime>
-            <EventInfo>
-              <BodyHeader>{name}</BodyHeader>
-              <BodyText>{description}</BodyText>
-            </EventInfo>
-          </EventRow>
+        {events.map((info) => (
+          <ScheduleRow info={info} />
         ))}
       </CardBase>
     );
@@ -54,35 +54,5 @@ const CardBase = styled('div')`
   :hover {
     transform: translateY(-10px);
     box-shadow: 0px 24px 48px rgba(0, 0, 0, 0.05);
-  }
-`;
-
-const EventRow = styled('div')`
-  display: flex;
-  line-height: 1.44em;
-
-  :first-child > div {
-    padding-top: 40px;
-  }
-  :last-child > div {
-    padding-bottom: 100px;
-  }
-`;
-
-const EventTime = styled('div')`
-  flex-basis: ${dateColSmWidth};
-  flex-grow: 0;
-  flex-shrink: 0;
-  overflow: hidden;
-
-  font-weight: 500;
-  font-size: 20px;
-  padding: 15px 25px;
-`;
-const EventInfo = styled('div')`
-  padding: 17px 35px;
-
-  @media (max-width: 700px) {
-    padding: 17px 20px;
   }
 `;
---
+++ src/sections/Schedule/ScheduleRow.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { BodyHeader, BodyText } from 'components/typography';
+
+import styled from 'styled-components';
+import palette from 'assets/palette';
+
+const ScheduleRow = ({ info }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <EventRow>
+      <EventTime>
+        {info.start}
+        <br />
+        {info.end}
+      </EventTime>
+      <EventInfo>
+        <BodyHeader>{info.name}</BodyHeader>
+        <BodyText>{info.description}</BodyText>
+        {info.additional && (
+          <div>
+            {isOpen && (
+              <BodyText style={{ marginTop: 10 }}>{info.additional}</BodyText>
+            )}
+            <p onClick={() => setIsOpen((v) => !v)}>
+              See {isOpen ? 'Less' : 'More'}
+            </p>
+          </div>
+        )}
+      </EventInfo>
+    </EventRow>
+  );
+};
+
+export default ScheduleRow;
+
+const dateColSmWidth = '120px';
+
+const EventRow = styled('div')`
+  display: flex;
+  line-height: 1.44em;
+
+  :first-child > div {
+    padding-top: 40px;
+  }
+  :last-child > div {
+    padding-bottom: 100px;
+  }
+`;
+
+const EventTime = styled('div')`
+  flex-basis: ${dateColSmWidth};
+  flex-grow: 0;
+  flex-shrink: 0;
+  overflow: hidden;
+
+  font-weight: 500;
+  font-size: 20px;
+  padding: 15px 25px;
+`;
+const EventInfo = styled('div')`
+  padding: 17px 35px;
+
+  @media (max-width: 700px) {
+    padding: 17px 20px;
+  }
+`;
